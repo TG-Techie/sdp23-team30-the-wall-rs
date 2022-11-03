c5483cc72635624b64b2a3d6ed896da19121705d
Jonah <tgtechie01@gmail.com>
2022-11-03 16:19:39 -0400

    pio blink working

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,24 @@ use panic_probe as _;
 
 use cortex_m as _;
 
-// import our bsp (Board support package) and alias it as bsp. Doing to lets us easily
-// change which bsp we use in the future.
+/* ========== [ BSP and related Items ] ========== */
+// The related items are for setting cpu speed, peripharls, etc. Here we rename the adafruit
+// BSP to "bsp"  so it is easy to switch BSPs if need be later.
 use adafruit_feather_rp2040 as bsp;
-
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
     pac,
     sio::Sio,
     watchdog::Watchdog,
 };
+
+/* ========== [ eeprom driver imports ] ========== */
+use pio_proc;
+use rp2040_hal::gpio::{FunctionPio0, Pin};
+use rp2040_hal::pio;
+use rp2040_hal::pio::PIOExt;
+
+/* ========== [ Main story / entry code ] ========== */
 
 #[entry]
 fn main() -> ! {
@@ -57,8 +65,33 @@ fn main() -> ! {
     // pin re-assignments (to handle switching between bsp)
     let led_pin = pins.d13;
 
-    // run
+    // setup the blink led
     let mut led_pin = led_pin.into_push_pull_output();
+
+    // setup the rp2040's pio
+    let _eeprom_pin: Pin<_, FunctionPio0> = pins.a0.into_mode();
+    let eeprom_pin_id: u8 = 26;
+
+    let program = pio_proc::pio_asm!(
+        ".wrap_target",
+        "set pins 0b1",
+        "set pins 0b0",
+        ".wrap",
+        // select_program("eeprom_11xx"), // Optional if only one program in the file
+        // options(max_program_size = 32)
+    );
+
+    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
+    let installed = pio.install(&program.program).unwrap();
+    let (mut sm, _, _) = rp2040_hal::pio::PIOBuilder::from_program(installed)
+        .set_pins(eeprom_pin_id, 1)
+        .clock_divisor(65536.0)
+        .build(sm0);
+    // The GPIO pin needs to be configured as an output.
+    sm.set_pindirs([(eeprom_pin_id, pio::PinDir::Output)]);
+    sm.start();
+
+    // run
 
     loop {
         info!("on!");
