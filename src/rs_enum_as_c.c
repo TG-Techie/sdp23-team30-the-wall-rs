bd455dce02403debe88f0ed16de0f031f8cff33a
Jonah <tgtechie01@gmail.com>
2022-11-21 19:33:09 -0500

    Start on UNI/O interface Code

---
+++ src/rs_enum_as_c.c
@@ -0,0 +1,18 @@
+
+
+typedef struct {
+    int* io_register;
+} Pin;
+
+typedef enum {
+    OpenID = 0,
+    DownID = 1,
+} PinWrapperVariantID_t
+
+    typedef union {
+    struct {
+        int ID;
+
+    } Open;
+
+} PinWrapper_t;
--- src/unio_bang.rs
+++ src/unio_bang.rs
@@ -1,6 +1,49 @@
-use rp2040_hal::gpio::{Input, Output, Pin, PinId, PullUp, PushPull};
+use rp2040_hal::gpio::{
+    Input, Output, Pin, PinId, PinMode, PullUp, PullUpInput, PushPull, ValidPinMode,
+};
 
-enum PinWrapper<P: PinId> {
-    open { p: Pin<P, Input<PullUp>> },
-    down { p: Pin<P, Output<PushPull>> },
+enum OpenDrainPin<P: PinId> {
+    PulledUp(Pin<P, PullUpInput>),
+    Low(Pin<P, Output<PushPull>>),
+}
+
+pub struct UNIOBang<P: PinId> {
+    pin: OpenDrainPin<P>,
+}
+
+impl<P: PinId> OpenDrainPin<P> {
+    fn new_init<S: Into<Pin<P, Input<PullUp>>>>(some_pin: S) -> Self {
+        let mut pin: Pin<P, PullUpInput> = some_pin.into();
+
+        //
+        pin.set_output_enable_override(rp2040_hal::gpio::OutputEnableOverride::Invert);
+
+        // Turn on the pullup
+        use crate::there_be_dragons::SetPulls;
+        let _pulls = pin.as_pulls().unwrap().set_pull_up(true);
+
+        Self::PulledUp(pin)
+    }
+
+    fn set_pulled_up(&mut self) -> Result<(), ()> {
+        Err(())
+    }
+
+    fn set_low(&mut self) -> Result<(), ()> {
+        Err(())
+    }
+}
+
+
+impl<P: PinId> UNIOBang<P> {
+    pub fn new<S: Into<Pin<P, Input<PullUp>>>>(some_pin: S) -> Self {
+        return Self {
+            pin: OpenDrainPin::new_init(some_pin),
+        };
+    }
+
+    pub write_word(&mut self, word: u8) -> Result<(), ()> {
+        self.pin.set_open();
+        Err(())
+    }
 }
